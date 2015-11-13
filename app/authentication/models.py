30b429f3f4742d261620e2177180ad35ff8bf649
Adolfo Tamayo <adolfo@inka-labs.com>
2015-11-13 10:27:50 -0500

    Frontend Base

--- app/authentication/models.py
+++ app/authentication/models.py
@@ -9,28 +9,25 @@ from itsdangerous import (TimedJSONWebSignatureSerializer
 class User(db.Model):
 
     __tablename__ = 'auth_user'
-    
-    id       = db.Column('id', db.Integer, primary_key=True, \
-                autoincrement='ignore_fk')
+
+    id = db.Column('id', db.Integer, primary_key=True,
+                   autoincrement='ignore_fk')
     username = db.Column(db.String(128),  nullable=False, unique=True)
-    email    = db.Column(db.String(128),  nullable=False, unique=True)
+    email = db.Column(db.String(128),  nullable=False, unique=True)
     password = db.Column(db.String(128),  nullable=False)
-    role     = db.Column(db.SmallInteger, nullable=False)
-    status   = db.Column(db.SmallInteger, nullable=False)
+    role = db.Column(db.SmallInteger, nullable=False)
+    status = db.Column(db.SmallInteger, nullable=False)
 
-
-    def __init__(self, username, email, password,role,status):
+    def __init__(self, username, email, password, role, status):
 
         self.username = username.title()
-        self.email    = email.lower()
+        self.email = email.lower()
         self.password = generate_password_hash(password)
-        self.role     = role
-        self.status   = status
-
+        self.role = role
+        self.status = status
 
     def __repr__(self):
         return '<User %r>' % (self.username)
-
 
     def check_password(self, password):
         return check_password_hash(self.password, password)
@@ -65,8 +62,8 @@ class User(db.Model):
             return None # invalid token
         user = User.query.get(data['id'])
         return user
-    
-    
+
+
     @staticmethod
     def verify_token_email(token):
         s = Serializer(current_app.config['SECRET_KEY'])
