2683c8f23997b5af0538091aa4bb4a864b37935e
Adolfo Tamayo <adolfo@inka-labs.com>
2015-11-20 19:49:41 -0500

    Finished main admin page, changing menu on all pages

--- web/dart/index.dart
+++ web/dart/index.dart
@@ -1,3 +1,41 @@
+import 'dart:html';
+import 'dart:math' show Random;
+import 'dart:convert' show JSON;
+import 'dart:async' show Future;
+
+class Admin {
+    String info_url = "http://demo7492033.mockable.io/models/";
+    List models;
+    Admin(callback){
+        HttpRequest.getString(info_url)
+            .then((String url_content){
+                this.models = JSON.decode(url_content);
+                callback();
+            })
+            .catchError((Error error){
+                print(error.toString());
+            });
+    }
+    setAdminContent(){
+        for(int i = 0; i<models.length; i++){
+            var model = models[i];
+            print('#model_' + (i+1).toString() + '_total_obj');
+            querySelector('#model_' + (i+1).toString() + '_total_obj').text = model["total_objects"];
+            querySelector('#model_' + (i+1).toString() + '_name').text = model["name"];
+            //querySelector('#model_' + (i+1).toString() + '_url').href = model["url"] ;
+
+        }
+    }
+
+}
+var admin;
+
+adminReady(){
+    print(admin.info_url);
+    print(admin.models);
+    admin.setAdminContent();
+}
+
 main() {
-    print("hola");
+    admin = new Admin(adminReady);
 }
