d8c21d4702514e43063f80f9aacb0d2726cffc33
daniel_lozano <daniel_lozano@ucsp.edu.pe>
2015-11-28 12:32:42 -0500

    Fixed PEP8 conventions

--- app/article/forms.py
+++ app/article/forms.py
@@ -1,11 +1,13 @@
-from flask.ext.wtf import Form # , RecaptchaField
-from wtforms import TextField, PasswordField, SubmitField  # BooleanField
-from wtforms import TextAreaField, HiddenField, validators
+from flask.ext.wtf import Form  # RecaptchaField
+from wtforms import TextField  # BooleanField
+from wtforms import TextAreaField, HiddenField
 from wtforms.ext.sqlalchemy.fields import QuerySelectField
-from wtforms.validators import Required, Email, EqualTo, Length, Regexp
+from wtforms.validators import Required
 from app.sections.models import Sections
 
-strip_filter = lambda x: x.strip() if x else None
+
+def strip_filter(x):
+    x.strip() if x else None
 
 
 def section_choice():
@@ -13,19 +15,19 @@ def section_choice():
 
 
 class ArticleCreateForm(Form):
-    title     = TextField('Title', [Required("Please enter a title")],
-                        filters=[strip_filter])
-    body      = TextAreaField('Body',[Required("Please enter a body")],
-                        filters=[strip_filter])
-    section   = QuerySelectField('Section', query_factory=section_choice)
+    title = TextField('Title', [Required("Please enter a title")],
+                      filters=[strip_filter])
+    body = TextAreaField('Body', [Required("Please enter a body")],
+                         filters=[strip_filter])
+    section = QuerySelectField('Section', query_factory=section_choice)
     user_name = HiddenField()
 
 
 class ArticleUpdateForm(Form):
-    title       = TextField('Title', [Required("Please enter a title")],
-                        filters=[strip_filter])
-    body        = TextAreaField('Body',[Required("Please enter a body")],
-                        filters=[strip_filter])
-    section     = QuerySelectField('Section', query_factory=section_choice)
-    user_name   = HiddenField()
-    id_article  = HiddenField()
+    title = TextField('Title', [Required("Please enter a title")],
+                      filters=[strip_filter])
+    body = TextAreaField('Body', [Required("Please enter a body")],
+                         filters=[strip_filter])
+    section = QuerySelectField('Section', query_factory=section_choice)
+    user_name = HiddenField()
+    id_article = HiddenField()
